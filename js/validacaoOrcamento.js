fd7040034ff633f0b26a4d81f8379e9d4bbe3461
Bruna <bruna.pe13@hotmail.com>
2021-04-11 23:59:45 -0300

    projeto bruna

--- js/validacaoOrcamento.js
+++ js/validacaoOrcamento.js
@@ -5,5 +5,26 @@ function validarOrcamento()
         alert ("Nome não informado");
         nome.focus ();
     }
+    var email =formulario.email;
+    if(email.value ==""){
+        alert ("Email não informado!");
+        email.focus();
+    }
+
+    var telefone = formulario.telefone;
+    if(telefone.value ==""){
+        alert("Telefone não informado!");
+        telefone.focus();
+    }
+    var aparelhos = formulario.aparelhos;
+    if(aparelhos.value ==""){
+        alert ("Aparelho não selecionado!");
+        aparelhos.focus();
+    }
+    var problema = formulario.problema;
+    if(problema.value==""){
+        alert("Problema não selecionado!");
+        problema.focus();
+    }
 
 }
